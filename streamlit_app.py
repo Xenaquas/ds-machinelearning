8c63cbd63e935280487423e14e4a08eac6136189
Xenaquas <136635514+Xenaquas@users.noreply.github.com>
2024-07-31 23:56:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🤖 Machine Learning App')
 
 st.info('This apps built a machine learning model')
+
+with st.expander('Data'):
+  st.write('Raw Data')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
