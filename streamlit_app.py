ffdf168c8d632c4b4d0506859d8bb7c7331a849b
Xenaquas <136635514+Xenaquas@users.noreply.github.com>
2024-07-31 23:58:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,10 @@ with st.expander('Data'):
   st.write('Raw Data')
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
   df
+
+  st.write('**X**)
+  X = df.drop['species', axis=1]    
+  X
+  st.write(**y**)
+  y = df.species
+  y
