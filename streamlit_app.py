6ea02f93950c903f212bdd8a326063a3d12a611d
Xenaquas <136635514+Xenaquas@users.noreply.github.com>
2024-08-01 11:00:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -90,6 +90,31 @@ df_prediction_proba.rename(columns = {0: 'Adelie',
 
 # Display predicted species
 st.subheader('Predicted Species')
+st.dataframe(df_prediction_proba,
+            column_config = {
+              'Adelie': st.column_config.ProgressColumn(
+                'Adelie',
+                format= '%f',
+                width= 'medium', 
+                min_value= 0, 
+                max_value=1      
+              ),
+              'Chinstrap': st.column_config.ProgressColumn(
+                'Chinstrap',
+                format= '%f',
+                width= 'medium', 
+                min_value= 0, 
+                max_value=1      
+              ),
+              'Gentoo': st.column_config.ProgressColumn(
+                'Gentoo',
+                format= '%f',
+                width= 'medium', 
+                min_value= 0, 
+                max_value=1      
+              )
+            }, hide_index=True)
+
 df_prediction_proba
 penguin_species = np.array(['Adelie', 'Chinstrap', 'Gentoo'])
 st.success(str(penguin_species[prediction][0]))
